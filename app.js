44ab456583a2f1a722a5655ef78017a75d5c7904
lalawow <lalawow@gmail.com>
2018-05-12 11:23:07 +0800

    proxy is working

---
+++ app.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let proxy = require('http-proxy-middleware');
+let path = require('path');
+let app = express()
+require('dotenv').load();
+
+
+
+
+
+app.use('/api', proxy({ target: process.env.PROXY_TARGET, changeOrigin: true }));
+app.use('/images/uploads', proxy({ target: process.env.PROXY_TARGET, changeOrigin: true }));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.listen(process.env.PORT_NUMBER)
+module.exports = app;
