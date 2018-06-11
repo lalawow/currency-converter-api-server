470bca8f2625634a9260cbb7be10ce88e921fcff
lalawow <lalawow@gmail.com>
2018-06-11 22:18:12 +0800

    增加静态文件缓存设置

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let proxy = require('http-proxy-middleware');
 let path = require('path');
 let app = express()
 require('dotenv').load();
+const options = {
+    maxAge: '1h'
+}
 
 
 
@@ -11,7 +14,7 @@ require('dotenv').load();
 app.use('/api', proxy({ target: process.env.PROXY_TARGET, changeOrigin: true }));
 app.use('/images/uploads', proxy({ target: process.env.PROXY_TARGET, changeOrigin: true }));
 app.use('/images/optimized', proxy({ target: process.env.PROXY_TARGET, changeOrigin: true }));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')), options);
 
 app.listen(process.env.PORT_NUMBER)
 module.exports = app;
